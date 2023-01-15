acf54bdbd71211c889a716c3f325bc0e35371cea
arsalankhan-17 <arsalankhan4014@gmail.com>
2023-01-15 21:37:25 +0500

    routing section completed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,7 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/about"
+      <router-link class="text-white font-bold uppercase text-2xl mr-4" :to="{name:'home'}"  exact-active-class="no-active"
       >Music</router-link
       >
 
@@ -10,15 +10,18 @@
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{name:'about'}">About</router-link>
+          </li>
           <li v-if="!userStore.userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal">Login / Register</a>
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" :to="{name:'manage'}">Manage</router-link>
             </li>
             <li>
-              <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">LogOut</a>
+              <a class="px-2 text-white" href="#" @click.prevent="signOut">LogOut</a>
             </li>
           </template>
 
@@ -40,6 +43,12 @@ export default {
   methods : {
     toggleAuthModal(){
       this.modalStore.isOpen  = !this.modalStore.isOpen;
+    },
+    signOut(){
+      this.userStore.signOut();
+      if(this.$route.meta.requiresAuth){
+        this.$router.push({name:'home'});
+      }
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import useUserStore from '@/stores/user.js'
+
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Manage from '../views/Manage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  linkExactActiveClass:'text-yellow-500',
   routes: [
     {
       path: '/',
@@ -14,8 +18,41 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: About
-    }
+    },
+    {
+      path: '/manage-music',
+      name: 'manage',
+      component: Manage,
+      meta: { requiresAuth: true }
+
+    },
+    {
+      path: '/manage',
+      redirect:{name:'manage'},
+    },
+    {
+      path: '/:catchAll(.*)*',
+      redirect:{name:'home'}
+    },
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+
+
+if(!to.meta.requiresAuth){
+  next();
+  return;
+}
+
+const store = useUserStore();
+
+if(store.userLoggedIn){
+  next();
+}else{
+  next({name:'home'});
+}
+
+});
 
 export default router
